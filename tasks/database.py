9b18551478644100c234489d0cbfb7f6bee023c7
wuhanchu <whcwuhanchu@gmail.com>
2020-04-07 16:35:27 +0800

    update

--- tasks/database.py
+++ tasks/database.py
@@ -34,7 +34,7 @@ def api_init(app, table_list):
         # permission
         for method in ["GET", "POST", "DELETE", "PATCH", "PUT"]:
             key = table.get("key") + "_" + method.lower()
-            if not Permission.query.filter_by(key=key).first():
+            if not Permission.query.filter_by(product_key=product_key, key=key).first():
                 db.session.add(
                     Permission(product_key=product_key, name=table.get("name") + "_" + method_map.get(method),
                                url="/" + table.get("key"),
@@ -43,7 +43,7 @@ def api_init(app, table_list):
 
         # permission scope
         parent_key = table.get("key") + "_" + "get"
-        if not PermissionScope.query.filter_by(key=parent_key).first():
+        if not PermissionScope.query.filter_by(product_key=product_key, key=parent_key).first():
             db.session.add(
                 PermissionScope(product_key=product_key, name=table.get("name"),
                                 key=parent_key, parent_key="login"))
@@ -54,7 +54,7 @@ def api_init(app, table_list):
         #  permission method scope
         for method in ["POST", "DELETE", "PATCH", "EXPORT", "IMPORT"]:
             key = table.get("key") + "_" + method.lower()
-            if not PermissionScope.query.filter_by(key=key).first():
+            if not PermissionScope.query.filter_by(product_key=product_key, key=key).first():
                 db.session.add(
                     PermissionScope(product_key=product_key, name=table.get("name") + "_" + method_map.get(method),
                                     key=table.get("key") + "_" + method.lower(), parent_key=parent_key))
@@ -68,13 +68,15 @@ def api_init(app, table_list):
                 elif method == "IMPORT":
                     permission_key = table.get("key") + "_" + "post"
 
-                db.session.add(PermissionScopeDetail(permission_key=permission_key, permission_scope_key=key))
+                db.session.add(PermissionScopeDetail(product_key=product_key, permission_key=permission_key,
+                                                     permission_scope_key=key))
 
     if app and app.url_map:
         for record in app.url_map.iter_rules():
             for method in record.methods:
                 if method in method_map.keys():
                     if len(record.rule) > 1 and Permission.query.filter_by(
+                            product_key=product_key,
                             key=record.rule[1:].replace("/", "_") + "_" + method.lower()).count() < 1:
                         db.session.add(
                             Permission(product_key=product_key, url=record.rule,
