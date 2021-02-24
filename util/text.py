f48ca74fbf74f7837ffcd710c4164bb246e559e1
wuhanchu <whcwuhanchu@gmail.com>
2021-02-24 14:42:32 +0800

    修改数据

--- util/text.py
+++ util/text.py
@@ -7,6 +7,9 @@ import re
 
 def remove_punctuation(text):
     """去除所有标点符号"""
+    if not text:
+        return text
+
     result = text.strip()
     result = re.sub(f"[{hanzi.punctuation}]", "", result)
     result = re.sub(f"[{string.punctuation}]", "", result)
