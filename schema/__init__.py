d41792ee5069bfe4f8d875ada1bbdf8e202c7b43
chenxuejun <chenxuejun@dataknown.cn>
2021-06-30 11:18:37 +0800

    BaseSchema对字符串参数strip

--- schema/__init__.py
+++ schema/__init__.py
@@ -1,7 +1,14 @@
 from flask_marshmallow import Schema
-from marshmallow import INCLUDE
+from marshmallow import INCLUDE, pre_load
 
 
 class BaseSchema(Schema):
     class Meta:
         unknown = INCLUDE
+
+    @pre_load()
+    def strip_string(self, data, **kwargs):
+        if isinstance(data, dict):
+            for key, value in data.items():
+                data[key] = value.strip() if isinstance(value, str) else value
+        return data
