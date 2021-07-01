65e28234aff0ec029278057952232914831301f4
chenxuejun <chenxuejun@dataknown.cn>
2021-07-01 08:53:17 +0800

    param pre_load 限制在asr模块

--- schema/__init__.py
+++ schema/__init__.py
@@ -5,10 +5,3 @@ from marshmallow import INCLUDE, pre_load
 class BaseSchema(Schema):
     class Meta:
         unknown = INCLUDE
-
-    @pre_load()
-    def strip_string(self, data, **kwargs):
-        if isinstance(data, dict):
-            for key, value in data.items():
-                data[key] = value.strip() if isinstance(value, str) else value
-        return data
