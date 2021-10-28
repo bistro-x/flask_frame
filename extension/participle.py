0f4a792055c51384f3e6142b3ae337105b9b0a2a
wuhanchu <whcwuhanchu@gmail.com>
2021-10-28 15:17:43 +0800

    更新

--- extension/participle.py
+++ extension/participle.py
@@ -21,7 +21,7 @@ def participle_sentence(sentence):
     global participle_app, participle_service_url
 
     if not participle_app or not participle_service_url:
-        raise ValueError("participle module not inited")
+        raise []
 
     try:
         response = requests.post(
