a8c7b0f46a888bf838b02a0a76ce9b8a032823ad
wuhanchu <whcwuhanchu@gmail.com>
2021-11-15 22:17:10 +0800

    black 格式化

--- extension/loguru/__init__.py
+++ extension/loguru/__init__.py
@@ -9,8 +9,14 @@ def _set_logger(app, config):
     # project
     from .compress import zip_logs
     from .macro import (
-        k_log_path, k_log_name, k_log_enqueue, k_log_format,
-        k_log_retention, k_log_rotation, k_log_serialize, k_log_level
+        k_log_path,
+        k_log_name,
+        k_log_enqueue,
+        k_log_format,
+        k_log_retention,
+        k_log_rotation,
+        k_log_serialize,
+        k_log_level,
     )
 
     path = config[k_log_name]
@@ -19,16 +25,17 @@ def _set_logger(app, config):
 
     app.logger.setLevel(config[k_log_level] or "ERROR")
 
-    logger.add(path,
-               level=(config[k_log_level] or "ERROR"),
-               format=config[k_log_format],
-               enqueue=config[k_log_enqueue],
-               serialize=config[k_log_serialize],
-               rotation=config[k_log_rotation],
-               retention=config[k_log_retention])
+    logger.add(
+        path,
+        level=(config[k_log_level] or "ERROR"),
+        format=config[k_log_format],
+        enqueue=config[k_log_enqueue],
+        serialize=config[k_log_serialize],
+        rotation=config[k_log_rotation],
+        retention=config[k_log_retention],
+    )
 
     class InterceptHandler(logging.Handler):
-
         def emit(self, record):
             # Get corresponding Loguru level if it exists
             try:
@@ -42,9 +49,11 @@ def _set_logger(app, config):
                 frame = frame.f_back
                 depth += 1
 
-            logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+            logger.opt(depth=depth, exception=record.exc_info).log(
+                level, record.getMessage()
+            )
 
-    gunicorn_logger = logging.getLogger('gunicorn.error')
+    gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
 
     for handler in list(app.logger.handlers):
@@ -57,22 +66,28 @@ def init_app(app):
     # project
     from .compress import zip_logs
     from .macro import (
-        k_log_path, k_log_name, k_log_enqueue, k_log_format,
-        k_log_retention, k_log_rotation, k_log_serialize, k_log_level
+        k_log_path,
+        k_log_name,
+        k_log_enqueue,
+        k_log_format,
+        k_log_retention,
+        k_log_rotation,
+        k_log_serialize,
+        k_log_level,
     )
 
-    config = {
-        "LOG_PATH": "./log",
-        "LOG_NAME": "{time:YYYY-MM-DD}.log"
-    }
+    config = {"LOG_PATH": "./log", "LOG_NAME": "{time:YYYY-MM-DD}.log"}
 
     config.setdefault(k_log_level, "ERROR")
     config.setdefault(k_log_path, None)
     config.setdefault(k_log_name, "")
-    config.setdefault(k_log_format, "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> "
-                                    "| <level>{level: <8}</level> | "
-                                    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> -"
-                                    " <level>{message}</level>")
+    config.setdefault(
+        k_log_format,
+        "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> "
+        "| <level>{level: <8}</level> | "
+        "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> -"
+        " <level>{message}</level>",
+    )
     config.setdefault(k_log_enqueue, True)
     config.setdefault(k_log_serialize, False)
     config.setdefault(k_log_rotation, "12:00")
