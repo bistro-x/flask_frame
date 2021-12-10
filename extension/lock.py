a4ff4f9d7196e0b58d44aa7122645b4dc58190af
chenxuejun <chenxuejun@dataknown.cn>
2021-12-10 11:20:23 +0800

    update 支持redis断开重连

--- extension/lock.py
+++ extension/lock.py
@@ -1,4 +1,5 @@
 import redis
+import platform
 
 from ..util.lock import FileLock
 
@@ -12,8 +13,26 @@ def init_app(app):
 
     lock_index = app.config.get("PRODUCT_KEY")
 
+    import socket
+
     if app.config.get("REDIS_URL"):
-        redis_client = redis.from_url(app.config.get("REDIS_URL"))
+
+        if platform.system() == "Linux":
+            socket_keepalive_options = {
+                socket.TCP_KEEPIDLE: 60,
+                socket.TCP_KEEPINTVL: 30,
+                socket.TCP_KEEPCNT: 3,
+            }
+        else:
+            socket_keepalive_options = None
+
+        redis_client = RedisClient(
+            app.config.get("REDIS_URL"),
+            decode_responses=True,
+            socket_keepalive=True,
+            socket_keepalive_options=socket_keepalive_options,
+        )
+
         lock_type = "redis_lock"
         app.logger.info("extension.lock use redis lock")
 
@@ -34,12 +53,52 @@ class Lock:
         if lock_index:
             lock_name = f"{lock_index}.{lock_name}"
 
-        return redis_client.lock(lock_name, timeout=timeout)
+        return redis_client.client.lock(lock_name, timeout=timeout)
 
     @staticmethod
     def lock_type():
         global lock_type
         return lock_type
+
+
+class RedisClient:
+
+    def __init__(self, url, decode_responses=True, socket_keepalive=True, socket_keepalive_options=None, max_retry=5):
+        self.url = url
+        self.decode_responses = decode_responses
+        self.socket_keepalive = socket_keepalive
+        self.socket_keepalive_options = socket_keepalive_options
+        self.max_retry = max_retry
+        self._client = None
+        self.init_client()
+
+    def init_client(self):
+        self._client = redis.Redis.from_url(
+            self.url,
+            decode_responses=self.decode_responses,
+            socket_keepalive=self.socket_keepalive,
+            socket_keepalive_options=self.socket_keepalive_options
+        )
+
+    @property
+    def client(self):
+        """
+        每次调用前ping()检测链接, 异常重新初始化客户端
+        """
+        _client = None
+
+        for _ in range(self.max_retry):
+            try:
+                self._client.ping()
+                _client = self._client
+            finally:
+                if _client:
+                    return _client
+                else:
+                    self.init_client()
+                    time.sleep(0.1)
+
+        return _client
 
 
 def get_lock(lock_name, timeout=600):
