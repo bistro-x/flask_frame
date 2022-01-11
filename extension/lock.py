2f11aed52ed9bd62de0ee4c38f3b0f4c9e403023
chenxuejun <chenxuejun@dataknown.cn>
2022-01-11 20:57:11 +0800

    redis集群支持密码

--- extension/lock.py
+++ extension/lock.py
@@ -35,19 +35,18 @@ def init_app(app):
 
         # redis 主从集群 master name
         redis_master_name = app.config.get("REDIS_MASTER_NAME", None)
-        redis_password = app.config.get("REDIS_PASSWORD", None)
-        sentinel_options = {}
-        if redis_master_name:
-            sentinel_options.update(service_name=redis_master_name)
-        if redis_password:
-            sentinel_options.update(password=redis_password)
 
         if redis_url.startswith("sentinel"):
+            sentinel_options = {"service_name": redis_master_name}
             sentinels = []
+            password = None
             urls = redis_url.split(";")
             for url in urls:
                 url = urlparse(url)
+                password = url.password
                 sentinels.append((url.hostname, url.port))
+            if password:
+                sentinel_options.update(password=password)
             redis_client = Sentinel(
                 sentinels,
                 decode_responses=True,
