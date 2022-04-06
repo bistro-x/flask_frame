a5694972e2afe5f557d3208f4e6d1fbb5baa2af5
wuhanchu <wuhanchu@dataknown.cn>
2022-04-06 10:09:14 +0800

    修改 api_log 逻辑

--- extension/api_log/__init__.py
+++ extension/api_log/__init__.py
@@ -35,19 +35,24 @@ def init_app(app):
         api_log.path = request.path
         api_log.headers = dict(request.headers)
         api_log.args = dict(request.args)
-        api_log.status = str(response.status_code)
+        api_log.status = response.status_code
         db.session.merge(api_log)
         db.session.commit()
 
         return response
-    
+
+
 if celery:
     from extension.celery import BaseTask
 
     @celery.task(base=BaseTask, name="api_log_clean")
     def api_log_clean():
-        """API日志清理
-        """
+        """API日志清理"""
         from frame.extension.database import db
-        api_log_retention_days = flask_app.config.get("API_LOG_RETENTION_DAYS", 30) # 日志保留时间
-        db.session.execute(f"delete from z_know_info.api_log where create_time < (now() - interval '{api_log_retention_days} day');"  )
+
+        api_log_retention_days = flask_app.config.get(
+            "API_LOG_RETENTION_DAYS", 30
+        )  # 日志保留时间
+        db.session.execute(
+            f"delete from z_know_info.api_log where create_time < (now() - interval '{api_log_retention_days} day');"
+        )
