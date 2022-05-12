5980baaa22b0d2a0b6bbfaf445fb05b4086471f4
uglgxu <wuhanchu@me.com>
2022-05-12 17:26:15 +0800

    修改发布逻辑

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-from pkg_resources import parse_requirements
-from setuptools import setup
+import  pkg_resources,pathlib
+from setuptools import setup,find_packages
 
-install_reqs = parse_requirements('./requirements.txt')
-print("install_reqs",install_reqs)
-reqs = [str(ir.req) for ir in install_reqs]
+# install_reqs = parse_requirements('./requirements.txt')
+# print("install_reqs",install_reqs)
+# reqs = [str(ir.req) for ir in install_reqs]
 
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+print(install_requires)
 
 setup(
     name='flask_rest_frame',
@@ -12,6 +20,6 @@ setup(
     author='wuhanchu',
     author_email='whcwuhanchu@gmail.com',
     description=u'基于FLASK快速开发REST接口框架',
-    packages=['jujube_pill'],
-    install_requires=reqs
+    packages= find_packages(),
+    install_requires=install_requires
 )
