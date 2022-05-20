29207d7818bfc79e29f442d6260fcfe13d04bfb7
uglgxu <wuhanchu@me.com>
2022-05-20 17:18:20 +0800

    剔除 无关的用户模块代码

--- src/flask_frame/extension/permission.py
+++ src/flask_frame/extension/permission.py
@@ -2,36 +2,22 @@ from http import HTTPStatus
 
 import flask
 import requests
-from authlib.integrations.flask_oauth2 import ResourceProtector
-from authlib.oauth2.rfc6750 import BearerTokenValidator
 from flask import request, abort, g
 
-require_oauth = ResourceProtector()
 app = None
 
 # 配置参数
 check_api = True  # 检查API权限
 fetch_user = True  # 是否获取用户
-get_user_extend_info = None  # 获取用户权限信息
 
 
 def fetch_current_user(token_string):
     """
-    todo 使用缓存
-    获取当前用户
+    查询当前用户
     :param token_string:
     :return:
     """
     global app
-
-    from authlib.integrations.flask_oauth2 import current_token
-
-    user_auth_local = app.config.get("USER_AUTH_LOCAL")
-
-    if current_token and current_token.user and user_auth_local:
-        from module.user.service import get_user_extend_info
-
-        return get_user_extend_info(current_token.user)
 
     # get users
     user_auth_url = app.config.get("USER_AUTH_URL")
@@ -50,9 +36,24 @@ def fetch_current_user(token_string):
                 ),
             },
         )
-        return response.json()
+
+        # 获取用户设置到全局中
+        user = response.json()
+        g.current_user = user
+        return user
     except requests.exceptions.RequestException as e:
         app.logger.exception(e)
+
+
+def get_current_user():
+    """获取当前用户"""
+    if not flask.has_request_context():
+        return None
+
+    if hasattr(g, "current_user"):
+        return g.current_user
+
+    return None
 
 
 def license_check():
@@ -74,91 +75,60 @@ def license_check():
         app.logger.exception(e)
 
 
-def config_oauth(app):
-    # protect resource
-    require_oauth.register_token_validator(_BearerTokenValidator())
-
-
 def check_user_permission(token_string=None):
     """
-    check current token
-    :param token_string:
-    :return:
+    权限检测
+    :param token_string: token信息
+    :return: 用户信息
     """
-    method = request.method
 
-    user_auth_local = app.config.get("USER_AUTH_LOCAL")
-
-    if user_auth_local and request.url_rule:
-        return True
-
-    if user_auth_local:
-        from module.auth.extension.oauth2 import require_oauth
-
-        @require_oauth('profile')
-        def local_oauth():
-            pass
-
-        local_oauth()
-
-    # get current user
-    if not user_auth_local:
-
-        # get users
-        user_auth_url = app.config.get("USER_AUTH_URL")
-        if not user_auth_url:
-            return {}
-
-        # 证书校验
-        license_check()
+    # 请求方法
 
     # 权限验证
-    response = fetch_current_user(token_string)
-    user = response if response else None
+    user = fetch_current_user(token_string)
+    return check_url_permission(user)
 
-    # 超级用户
-    g.current_user = user
+
+def check_url_permission(user):
+    """检测是否有相关接口权限
+
+
+    Args:
+        user (_type_): 检测用户
+    Returns:
+        _type_: 是否通过
+    """
+    # 没有用户信息直接返回
+    if not user:
+        return False
+
+    # 超级用户不需要检测权限
     if user and user.get("name") == "admin":
         return user
 
     check_path = request.url_rule.rule if request.url_rule else request.path
+    method = request.method
+
     if (
         user
-        and check_api
         and user.get("permissions")
         and any(
             permission.get("url") == check_path and permission.get("method") == method
             for permission in user.get("permissions")
         )
     ):
-        return user
+        return True
     elif check_path and check_path.startswith("/static/"):
         return True
 
-    return user
-
-
-class _BearerTokenValidator(BearerTokenValidator):
-    def authenticate_token(self, token_string):
-        check_user_permission(token_string)
-
-
-def get_current_user():
-    """获取当前用户"""
-    if not flask.has_request_context():
-        return None
-
-    if hasattr(g, "current_user"):
-        return g.current_user
-
-    return None
+    return False
 
 
 def param_add_department_filter(params={}):
     """
     添加组织权限过滤
     :param params: 当前参数
-    :return:
+    :return: 过滤参数
     """
     user = g.current_user
 
@@ -170,7 +140,7 @@ def param_add_department_filter(params={}):
         depart_str = ",".join(depart_list)
         params["or"] = f"(department_key.is.null,{depart_str})"
     else:
-        params["department_key"] = f"is.null"
+        params["department_key"] = "is.null"
 
     return params
 
@@ -181,7 +151,6 @@ def init_app(flask_app):
     app = flask_app
     check_api = app.config.get("CHECK_API", True)
     fetch_user = app.config.get("FETCH_USER", True)
-    get_user_extend_info =
 
     @app.before_request
     def app_proxy():
