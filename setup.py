ee8e7b98fe3c52f4ecad57fe478ae5dbbfd6f44f
uglgxu <wuhanchu@me.com>
2022-05-23 11:30:26 +0800

    修改版本

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with pathlib.Path("requirements.txt").open() as requirements_txt:
 
 setup(
     name="flask_frame",
-    version="0.2.1",
+    version="0.2.2",
     author="wuhanchu",
     author_email="whcwuhanchu@gmail.com",
     description=u"基于FLASK快速开发REST接口框架",
