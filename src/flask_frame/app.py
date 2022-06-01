191f82bfe1a0b47e19163323a4a6e9159e688495
uglgxu <wuhanchu@me.com>
2022-06-01 11:05:45 +0800

    修改数据

--- src/flask_frame/app.py
+++ src/flask_frame/app.py
@@ -56,6 +56,10 @@ def create_app(config, flask_config_name=None, config_custom=None, **kwargs):
     # 全局异常处理
     @app.errorhandler(Exception)
     def exception_handle(error):
+        from .extension.database import db
+
+        db.session.rollback()
+
         app.logger.exception(error)
 
         if isinstance(error, BusiError):
